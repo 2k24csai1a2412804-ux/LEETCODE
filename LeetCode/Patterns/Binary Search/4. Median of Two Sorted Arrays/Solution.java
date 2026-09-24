5ddfcd51acbffddfd95ae8e26412216c8e5b9abd
Anushthi Saxena <2k24.csai1a.2412804@gmail.com>
2026-09-24 21:59:06 +0530

    Add Hard Java Solution for 4. Median of Two Sorted Arrays from LeetCode

--- LeetCode/Patterns/Binary Search/4. Median of Two Sorted Arrays/Solution.java
+++ LeetCode/Patterns/Binary Search/4. Median of Two Sorted Arrays/Solution.java
@@ -56,3 +56,7 @@ class Solution {
         return 0.0;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
